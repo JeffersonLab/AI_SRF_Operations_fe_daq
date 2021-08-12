8563df1c58e804093d6cfec0894d0a41a7d19f5b
Adam Carpenter <adamc@jlab.org>
2021-08-12 11:50:50 -0400

    Add support for P1R and GSET=0 bypassing

--- src/cavity.py
+++ src/cavity.py
@@ -2,7 +2,7 @@ import epics
 import logging
 import numpy as np
 
-from state_monitor import connection_cb, rf_on_cb
+from state_monitor import connection_cb, rf_on_cb, StateMonitor
 
 logger = logging.getLogger(__name__)
 
@@ -33,13 +33,14 @@ class Cavity:
         self.pset = epics.PV(f"{self.epics_name}PSET", connection_callback=connection_cb)
         self.odvh = epics.PV(f"{self.epics_name}ODVH", connection_callback=connection_cb)
         self.pset_init = self.pset.get()
+        self.gset_init = self.gset.get()
 
         # Create "RF On" PV and min stable gradient setting for each type of cavity
-        if self.cavity_type == "C100" or self.cavity_type == "C75":
+        if self.cavity_type in ("C100", "C75", "P1R"):
             # 1 = RF on, 0 = RF off
             self.rf_on = epics.PV(f"{self.epics_name}RFONr", connection_callback=connection_cb)
             self.gset_min = 5
-        elif self.cavity_type == "C50" or self.cavity_type == "C25":
+        elif self.cavity_type in ("C50", "C25"):
             # 1 = RF on, 0 = RF off
             self.rf_on = epics.PV(f"{self.epics_name}ACK1.B6", connection_callback=connection_cb)
             self.gset_min = 3
@@ -50,6 +51,13 @@ class Cavity:
 
         self.pv_list = [self.gset, self.pset, self.odvh, self.rf_on]
 
+        # Cavity can be effectively bypassed in a number of ways.  Work through that here.
+        self.bypassed_eff = bypassed
+        if self.gset_init == 0:
+            self.bypassed_eff = True
+        elif self.odvh.value == 0:
+            self.bypassed_eff = True
+
     def get_jiggled_pset_value(self, delta: float) -> float:
         """Calculate a random.uniform offset from pset_init of maximum +/- 5.  No changes to EPICS"""
         return self.pset_init + np.random.uniform(-delta, delta)
@@ -58,8 +66,8 @@ class Cavity:
         """Return the appropriate lowest no FE gradient.  Either the lowest stable or the highest known without FE."""
         return self.gset_min if self.gset_no_fe is None else self.gset_no_fe
 
-    def set_gradient(self, gset: float):
-        if gset != 0 and self.bypassed:
+    def set_gradient(self, gset: float, settle_time: float = 6.0):
+        if gset != 0 and self.bypassed_eff:
             msg = f"{self.name}: Can't turn on bypassed cavity"
             logger.error(msg)
             raise ValueError(msg)
@@ -68,14 +76,21 @@ class Cavity:
             logger.error(msg)
             raise ValueError(msg)
         if gset > self.odvh.value:
-            msg = f"{self.name}: Can't turn cavity too far above operational max"
+            msg = f"{self.name}: Can't turn cavity above operational max (ODVH={self.odvh.value})"
             logger.error(msg)
             raise ValueError(msg)
 
+        # Instead of trying to watch tuner status, etc., we just set the gradient, then sleep some requested amount of
+        # time.  This will need to be approached differently if used in a more general purpose application.
         self.gset.put(gset, wait=True)
+        StateMonitor.monitor(duration=settle_time)
 
     def restore_pset(self):
-        self.pset.value = self.pset_init
+        self.pset.put(self.pset_init, wait=True)
+
+    def restore_gset(self):
+        self.set_gradient(self.gset_init)
+
 
     def wait_for_connections(self, timeout=2):
         """Wait for PVs to connect or timeout.  Then finish object initialization knowing PVs are connected.
