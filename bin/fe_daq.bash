bb9c42312dea89bde0610bf28125ea82746012cd
Adam Carpenter <adamc@jlab.org>
2021-08-25 17:53:44 -0400

    Initial commit - app launcher

---
+++ bin/fe_daq.bash
@@ -0,0 +1,11 @@
+#!/bin/env bash
+
+# Get the directory containing this script
+DIR="$( cd "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" >/dev/null 2>&1 && pwd )"
+
+# Make sure our package search path is right
+export PATH="/usr/csite/pubtools/python/3.7/bin:$PATH"
+export PYTHONPATH="${DIR}/../src/:${PYTHONPATH}"
+
+# Run the app passing along all of the args
+python3.7 ${DIR}/../src/main.py "$@" 
