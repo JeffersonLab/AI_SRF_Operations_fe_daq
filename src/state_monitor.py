12dd4f5938b0f1a751173a17a0b0e18e29e8cad2
Adam Carpenter <adamc@jlab.org>
2022-05-24 10:42:54 -0400

    Update error message to be clearer

--- src/state_monitor.py
+++ src/state_monitor.py
@@ -42,7 +42,7 @@ def hv_read_back_cb(pvname: str, value: float, **kwargs) -> None:
     # Nominally supposed to be ~1000V, but in practice the range is pretty broad
     if value < 850 or value > 1150:
         StateMonitor.hv_has_problem(pvname=pvname)
-        logger.error(f"{pvname} is wrong {value} (!= 1000 +/- 15%).")
+        logger.error(f"{pvname} value is out of spec.  ({value} != 1000 +/- 15%).")
     else:
         StateMonitor.hv_good(pvname=pvname)
         #logger.info(f"{pvname} is ok {value} (== 1000 +/- 10%).")
