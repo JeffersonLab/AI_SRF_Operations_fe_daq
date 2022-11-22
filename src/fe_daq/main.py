951e307611421073fa617e835afcd7d7aac714c5
Adam Carpenter <adamc@jlab.org>
2022-11-22 16:21:20 -0500

    Add testing param to Config object

--- src/fe_daq/main.py
+++ src/fe_daq/main.py
@@ -118,16 +118,18 @@ def main() -> int:
         linac_name = args.linac
         testing = args.testing
 
+        config_file = Config.app_root + "/fe_daq.cfg"
+        if os.path.isfile(config_file):
+            Config.parse_config_file(config_file)
+
         # Setup logging for the whole app
         dir_name = f"run-{linac_name}-{datetime.now().strftime('%Y-%m-%d_%H%M%S.%f')}"
         if testing:
             dir_name = f"run-testing-{linac_name}-{datetime.now().strftime('%Y-%m-%d_%H%M%S.%f')}"
+            Config.set_parameter('testing', True)
         log_dir = os.path.join(Config.app_root, "log", dir_name)
         init_logging(log_dir=log_dir, run_log="fe_daq.log")
 
-        config_file = Config.app_root + "/fe_daq.cfg"
-        if os.path.isfile(config_file):
-            Config.parse_config_file(config_file)
 
         logger = logging.getLogger(__name__)
 
