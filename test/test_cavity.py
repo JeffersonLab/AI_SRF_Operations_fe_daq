e44a5359c9de0fca75d92b956a85620607c57c60
Adam Carpenter <adamc@jlab.org>
2022-12-08 12:41:52 -0500

    Update to use config file for cav, zone, and linac monitoring

--- test/test_cavity.py
+++ test/test_cavity.py
@@ -16,26 +16,40 @@ from src.fe_daq.linac import Zone, Linac
 
 
 def setUpModule():
-    config.set_parameter("testing", True)
+    config.parse_config_file(config.app_root + "/test/dummy_fe_daq.json")
 
 
 logger = logging.getLogger()
 
 
 def get_cavity(controls_type='2.0', style_2='old'):
+    config.validate_config()
+    lp_max = config.get_parameter('linac_pressure_max')
+    lp_recovery_margin = config.get_parameter('linac_pressure_margin')
+    heater_capacity_min = config.get_parameter('cryo_heater_margin_min')
+    heater_recover_margin = config.get_parameter('cryo_heater_margin_recovery_margin')
+    jt_max = config.get_parameter('jt_valve_position_max')
+    jt_recovery_margin = config.get_parameter('jt_valve_margin')
+    # TODO: Add LLRF 1.0
     if controls_type == '2.0':
-        linac = Linac("NorthLinac", prefix="adamc:")
-        zone = Zone(name="1L22", linac=linac, controls_type='2.0')
-        cav = LLRF2Cavity(name="1L22-1", epics_name="adamc:R1M1", cavity_type="C100", length=0.7, bypassed=False, zone=zone,
-                     Q0=6e9)
+        tuner_recovery_margin = config.get_parameter('LLRF1_tuner_recovery_margin')
+        linac = Linac("NorthLinac", prefix="adamc:", linac_pressure_max=lp_max,
+                      linac_pressure_recovery_margin=lp_recovery_margin, heater_margin_min=heater_capacity_min,
+                      heater_recovery_margin=heater_recover_margin)
+        zone = Zone(name="1L22", linac=linac, controls_type='2.0', jt_max=jt_max, jt_recovery_margin=jt_recovery_margin)
+        cav = LLRF2Cavity(name="1L22-1", epics_name="adamc:R1M1", cavity_type="C100", length=0.7, bypassed=False,
+                          zone=zone, Q0=6e9, tuner_recovery_margin=tuner_recovery_margin)
         if style_2 == 'old':
             cav.fcc_firmware_version = 2018.0
         cav.update_gset_max()
     elif controls_type == '3.0':
-        linac = Linac("NorthLinac", prefix="adamc:")
-        zone = Zone(name="1L10", linac=linac, controls_type='3.0')
+        tuner_recovery_margin = config.get_parameter('LLRF2_tuner_recovery_margin')
+        linac = Linac("NorthLinac", prefix="adamc:", linac_pressure_max=lp_max,
+                      linac_pressure_recovery_margin=lp_recovery_margin, heater_margin_min=heater_capacity_min,
+                      heater_recovery_margin=heater_recover_margin)
+        zone = Zone(name="1L10", linac=linac, controls_type='3.0', jt_max=jt_max, jt_recovery_margin=jt_recovery_margin)
         cav = LLRF3Cavity(name="1L10-1", epics_name="adamc:R1A1", cavity_type="C75", length=0.4916, bypassed=False,
-                     zone=zone, Q0=6.3e9)
+                          zone=zone, Q0=6.3e9, tuner_recovery_margin=tuner_recovery_margin)
         cav.update_gset_max()
     else:
         raise RuntimeError("Unsupported controls_type")
